58d04ae59f5a1d2b7242b631319e54e90080f029
Leonardo Lopes <poderaospez@gmail.com>
2024-11-09 21:35:23 -0300

    update

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -4,6 +4,8 @@ import { setDoc, doc } from 'firebase/firestore'; // Firestore imports
 import { Form, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { auth, db } from '../../data/firebaseConfig'; // Firebase authentication and Firestore
+import InputMask from 'react-input-mask'; // Importando o InputMask
+
 import '../../style/Register.css';
 
 const RegisterForm = ({ onRegister }) => {
@@ -62,110 +64,121 @@ const RegisterForm = ({ onRegister }) => {
 
   return (
     <Form onSubmit={handleRegister} className="auth-form">
-      <Form.Group controlId="formName">
-        <Form.Label>Primeiro nome</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu primeiro nome"
-          value={Firstname}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formSecondname">
-        <Form.Label>Sobrenome</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu sobrenome"
-          value={Secondname}
-          onChange={(e) => setSecondName(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formCpf">
-        <Form.Label>CPF</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu CPF"
-          value={cpf}
-          onChange={(e) => setCpf(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formCep">
-        <Form.Label>CEP</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu CEP"
-          value={cep}
-          onChange={(e) => setCep(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formAddress">
-        <Form.Label>Endereço</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu endereço"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formPhone">
-        <Form.Label>Telefone/WhatsApp</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Digite seu telefone ou WhatsApp"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formEmailRegister">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Digite seu email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formPasswordRegister">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Digite sua senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      <Form.Group controlId="formConfirmPassword">
-        <Form.Label>Confirme sua senha</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Confirme sua senha"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-          className="auth-input"
-        />
-      </Form.Group>
-      {error && <p className="text-danger">{error}</p>}
-      <Button variant="success" type="submit" className="register-btn mt-3">
-        Registrar
-      </Button>
-    </Form>
+  <Form.Group controlId="formName">
+    <Form.Label>Primeiro nome</Form.Label>
+    <Form.Control
+      type="text"
+      placeholder="Digite seu primeiro nome"
+      value={Firstname}
+      onChange={(e) => setFirstName(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formSecondname">
+    <Form.Label>Sobrenome</Form.Label>
+    <Form.Control
+      type="text"
+      placeholder="Digite seu sobrenome"
+      value={Secondname}
+      onChange={(e) => setSecondName(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formCpf">
+    <Form.Label>CPF</Form.Label>
+    <InputMask
+      mask="999.999.999-99"
+      value={cpf}
+      onChange={(e) => setCpf(e.target.value)}
+      placeholder="Digite seu CPF"
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formCep">
+    <Form.Label>CEP</Form.Label>
+    <InputMask
+      mask="99999-999"
+      value={cep}
+      onChange={(e) => setCep(e.target.value)}
+      placeholder="Digite seu CEP"
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formAddress">
+    <Form.Label>Endereço</Form.Label>
+    <Form.Control
+      type="text"
+      placeholder="Digite seu endereço"
+      value={address}
+      onChange={(e) => setAddress(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formPhone">
+    <Form.Label>Telefone/WhatsApp</Form.Label>
+    <Form.Control
+      type="text"
+      placeholder="Digite seu telefone ou WhatsApp"
+      value={phone}
+      onChange={(e) => setPhone(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formEmailRegister">
+    <Form.Label>Email</Form.Label>
+    <Form.Control
+      type="email"
+      placeholder="Digite seu email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formPasswordRegister">
+    <Form.Label>Senha</Form.Label>
+    <Form.Control
+      type="password"
+      placeholder="Digite sua senha"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  <Form.Group controlId="formConfirmPassword">
+    <Form.Label>Confirme sua senha</Form.Label>
+    <Form.Control
+      type="password"
+      placeholder="Confirme sua senha"
+      value={confirmPassword}
+      onChange={(e) => setConfirmPassword(e.target.value)}
+      required
+      className="auth-input"
+    />
+  </Form.Group>
+
+  {error && <p className="text-danger">{error}</p>}
+
+  <Button variant="success" type="submit" className="register-btn mt-3">
+    Registrar
+  </Button>
+</Form>
+
   );
 };
 
